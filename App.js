901fde55c72db73774adea85a5f96624de7a8c48
Justin Perez <justin@startrev.com>
2020-02-11 01:26:19 -0800

    Feat: Add event handler to App and pass down to Search

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
+import Search from './src/Components/Search';
+
+export default class App extends React.Component {
+  handleSearchChange(text) {
+    console.log('search text is', text);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Search
+      onChange={text => this.handleSearchChange(text)}
+      />
     </View>
   );
 }
