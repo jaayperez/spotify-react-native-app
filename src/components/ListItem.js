ff57d13d2e96dfd272aa02ade494dc5f9ed6a4bd
Justin Perez <justin@startrev.com>
2020-02-11 02:09:18 -0800

    Build: Align ListItem parts with flexbox

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,24 @@
 import React, { Component } from 'react';
-import { View, Image, Text } from 'react-native';
+import {
+  View,
+  Image,
+  Text,
+  Stylesheet,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 10,
+  },
+  title: {},
+});
+
 
 export default ({
   item: {
@@ -7,8 +26,8 @@ export default ({
     title,
   }
 }) => (
-  <View>
-    <Image source={{ uri: imageUri }} style={{ width: 100, height: 100 }}/>
-    <Text>{title}</Text>
+  <View style={styles.container}>
+    <Image source={{ uri: imageUri }} style={styles.image}/>
+    <Text style={styles.title}>{title}</Text>
   </View>
 );
